5819c331d49570c06200f41d3da499e122d3b9a2
ronanren <renouxpro@gmail.com>
2020-03-31 22:00:47 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import requests
 import re
 
@@ -8,7 +9,13 @@ indexFrance = p.text.find(
     '<a class="mt_a" href="country/france/">France</a>')
 indexFranceEnd = indexFrance + p.text[indexFrance:].find("</tr>")
 
+PlaceInWorld = p.text[:indexFranceEnd].count('href="country/') + 1
+
 cases = re.findall(r'[\d]{1,3},[\d]{3}|\d{3}', str(
     p.text[indexFrance:indexFranceEnd]))
+data = ["Total Cases", "New Cases", "Total Deaths",
+        "New Deaths", "Total Recovered", "Active Cases", "Critical"]
 
-print('cas en 24h : ' + cases[3])
+print("Nous sommes " + str(PlaceInWorld) + "ème dans le monde")
+for i in range(0, 7):
+    print(data[i] + ": " + cases[i])

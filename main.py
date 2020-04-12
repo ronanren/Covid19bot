c436b00dc17b40f5f191681f6c4a99696959fa27
ronanren <renouxpro@gmail.com>
2020-04-12 14:08:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import re
 from datetime import date
 import matplotlib.pyplot as plt
 from math import ceil
-import csv, os, time, locale
+import csv
+import os
+import time
+import locale
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -16,7 +19,6 @@ import config
 locale.setlocale(locale.LC_TIME, '')
 spinner = Halo(text='Scrapping data', spinner='dots', color='cyan')
 spinner.start()
-
 
 
 def makeTabOfData():
@@ -58,6 +60,8 @@ def makeTabOfData():
 
 
 def makeGraph():
+
+    os.mkdir("data/" + str(date.today()))
     # Graph 1
     plt.plot(tabDate, tabTotalCases, "o-",
              label="Population touchée", linewidth=3, color="#9b59b6")
@@ -78,7 +82,8 @@ def makeGraph():
     plt.ylabel('Population')
     plt.title('Avancé du COVID-19 en France du ' +
               str(date.today().strftime("%A %d %B %Y")))
-    plt.savefig('data/' + str(date.today()) + '_1.png')
+    plt.savefig('data/' + str(date.today()) +
+                "/" + str(date.today()) + '_1.png')
     plt.clf()
     # Graph 2
     plt.plot(tabDate[1:], tabNewCases[1:], "o-",
@@ -101,30 +106,33 @@ def makeGraph():
     plt.ylabel('Population')
     plt.title('Avancé du COVID-19 en France du ' +
               str(date.today().strftime("%A %d %B %Y")))
-    plt.savefig('data/' + str(date.today()) + '_2.png')
+    plt.savefig('data/' + str(date.today()) +
+                "/" + str(date.today()) + '_2.png')
     plt.clf()
 
     # Graph 3
     index = p.text.find('id="main_table_countries_today"')
     indexEnd = index + p.text[index:].find("</table>")
 
-    countries = re.findall(r'(?<=<a class="mt_a" href="country\/)(.*)(?=\/">)', str(p.text[index:indexEnd]))
+    countries = re.findall(
+        r'(?<=<a class="mt_a" href="country\/)(.*)(?=\/">)', str(p.text[index:indexEnd]))
     countries = [country.upper() for country in countries][:5]
-
-
-    totalcases = re.findall(r'(?<=<\/a><\/td>\n<td style="font-weight: bold; text-align:right">)((.|\n){50})', str(p.text[index:indexEnd]))
+    totalcases = re.findall(
+        r'(?<=<\/a><\/td>\n<td style="font-weight: bold; text-align:right">)((.|\n){50})', str(p.text[index:indexEnd]))
 
     totalCasesCountries = []
-    totalCasesCountries = (re.findall(r"[\d]{1,5},[\d]{3}", str(totalcases)))[:5]
+    totalCasesCountries = (re.findall(
+        r"[\d]{1,5},[\d]{3}", str(totalcases)))[:5]
 
-    totaldeaths = re.findall(r'(?<=<\/a><\/td>\n<td style="font-weight: bold; text-align:right">)((.|\n){180})', str(p.text[index:indexEnd]))
+    totaldeaths = re.findall(
+        r'(?<=<\/a><\/td>\n<td style="font-weight: bold; text-align:right">)((.|\n){180})', str(p.text[index:indexEnd]))
     totalDeathsCountries = []
 
-    for x in range(0,5):
-        totalDeathsCountries.append(int(re.findall(r"[\d]{1,5},[\d]{3}", str(totaldeaths[x][0][100:]))[0].replace(",", "")))
+    for x in range(0, 5):
+        totalDeathsCountries.append(int(re.findall(
+            r"[\d]{1,5},[\d]{3}", str(totaldeaths[x][0][100:]))[0].replace(",", "")))
 
-
-    for x in range(0,5):
+    for x in range(0, 5):
         totalCasesCountries[x] = int(totalCasesCountries[x].replace(",", ""))
 
     year = countries[::-1]
@@ -133,18 +141,21 @@ def makeGraph():
 
     totalcases = [totalcases[i]-deaths[i] for i in range(5)]
 
-    bar1 = plt.barh(year, deaths, color="#2c3e50", label="Population décédée", height=0.8)  
+    bar1 = plt.barh(year, deaths, color="#2c3e50",
+                    label="Population décédée", height=0.8)
     # careful: notice "bottom" parameter became "left"
-    bar2 = plt.barh(year, totalcases, left=deaths, color="#9b59b6", label="Population malade", height=0.8)
+    bar2 = plt.barh(year, totalcases, left=deaths,
+                    color="#9b59b6", label="Population malade", height=0.8)
 
     # we also need to switch the labels
-    plt.xlabel('Nombre total de malade et décès')  
+    plt.xlabel('Nombre total de malade et décès')
     plt.ylabel('Pays')
 
     plt.legend(loc='lower right')
     plt.title('Avancé du COVID-19 dans le monde du ' +
               str(date.today().strftime("%A %d %B %Y")))
-    plt.savefig('data/' + str(date.today()) + '_3.png')
+    plt.savefig('data/' + str(date.today()) +
+                "/" + str(date.today()) + '_3.png')
     plt.clf()
 
 
@@ -155,13 +166,16 @@ while True:
     # Parse data on worldometers.info
     p = requests.get('https://www.worldometers.info/coronavirus/')
 
-    indexFrance = p.text.find('<a class="mt_a" href="country/france/">France</a>')
+    indexFrance = p.text.find(
+        '<a class="mt_a" href="country/france/">France</a>')
     indexFranceEnd = indexFrance + p.text[indexFrance:].find("</tr>")
 
     PlaceInWorld = p.text[:indexFranceEnd].count('href="country/')
 
-    cases = re.findall(r'[\d]{1,3}.[\d]{3}|\d+', str(p.text[indexFrance:indexFranceEnd]).replace(",", "."))
-    data = ["Total Cases", "New Cases", "Total Deaths", "New Deaths", "Total Recovered", "Active Cases", "Critical"]
+    cases = re.findall(
+        r'[\d]{1,3}.[\d]{3}|\d+', str(p.text[indexFrance:indexFranceEnd]).replace(",", "."))
+    data = ["Total Cases", "New Cases", "Total Deaths",
+            "New Deaths", "Total Recovered", "Active Cases", "Critical"]
     numberOfDay = (date.today()-date(2020, 3, 16)).days
 
     verif = False
@@ -176,22 +190,31 @@ while True:
             verif = True
         else:
             spinner.color = 'magenta'
-            spinner.text = 'Data already published ' + time.strftime("%H:%M:%S")
+            spinner.text = 'Data already published ' + \
+                time.strftime("%H:%M:%S")
     except:
         spinner.text = 'Data not yet published ' + time.strftime("%H:%M:%S")
 
     if (verif):
-        
-        newRecovered = int(cases[4].replace(".", "")) - int(last_line[5].replace(".", ""))
-        newCritical = int(cases[6].replace(".", "")) - int(last_line[7].replace(".", ""))
-        newActive = int(cases[5].replace(".", "")) - int(last_line[6].replace(".", ""))
-        newTests = int(cases[9].replace(".", "")) - int(last_line[12].replace(".", ""))
+
+        newRecovered = int(cases[4].replace(".", "")) - \
+            int(last_line[5].replace(".", ""))
+        newCritical = int(cases[6].replace(".", "")) - \
+            int(last_line[7].replace(".", ""))
+        newActive = int(cases[5].replace(".", "")) - \
+            int(last_line[6].replace(".", ""))
+        newTests = int(cases[9].replace(".", "")) - \
+            int(last_line[12].replace(".", ""))
 
         # percentage of new cases compared to yesterday's case
-        newRecoveredPercent = round(100 * (int(newRecovered)/int(last_line[8].replace(".", ""))), 2)
-        newActivePercent = round(100 * (int(newActive)/int(last_line[9].replace(".", ""))), 2)
-        newCriticalPercent = round(100 * (int(newCritical)/int(last_line[10].replace(".", ""))), 2)
-        newDeathPercent = round(100 * (int(cases[3].replace(".", ""))/int(last_line[4].replace(".", ""))), 2)
+        newRecoveredPercent = round(
+            100 * (int(newRecovered)/int(last_line[8].replace(".", ""))), 2)
+        newActivePercent = round(
+            100 * (int(newActive)/int(last_line[9].replace(".", ""))), 2)
+        newCriticalPercent = round(
+            100 * (int(newCritical)/int(last_line[10].replace(".", ""))), 2)
+        newDeathPercent = round(
+            100 * (int(cases[3].replace(".", ""))/int(last_line[4].replace(".", ""))), 2)
 
         # Save data in csv file
         f = open("data/dataFrance.csv", "a+")
@@ -206,22 +229,30 @@ while True:
         msg = MIMEMultipart()
         msg['Subject'] = 'Data bot'
         ligne1 = "La 🇫🇷 est " + str(PlaceInWorld) + "ème au 🌎\n"
-        ligne2 = "🟢 " + cases[4].replace(".", ",") + " guéris +" + str(newRecovered) + " [" + str(newRecoveredPercent) + "%]\n"
+        ligne2 = "🟢 " + cases[4].replace(".", ",") + " guéris +" + str(
+            newRecovered) + " [" + str(newRecoveredPercent) + "%]\n"
         if (newActive < 0):
-            ligne3 = "🟠 " + cases[5].replace(".", ",") + " malades " + str(newActive) + " [" + str(newActivePercent) + "%]\n"
+            ligne3 = "🟠 " + cases[5].replace(".", ",") + " malades " + str(
+                newActive) + " [" + str(newActivePercent) + "%]\n"
         else:
-            ligne3 = "🟠 " + cases[5].replace(".", ",") + " malades +" + str(newActive) + " [" + str(newActivePercent) + "%]\n"
+            ligne3 = "🟠 " + cases[5].replace(".", ",") + " malades +" + str(
+                newActive) + " [" + str(newActivePercent) + "%]\n"
         if (newCritical < 0):
-            ligne4 = "🔴 " + cases[6].replace(".", ",") + " cas graves " + str(newCritical) + " [" + str(newCriticalPercent) + "%]\n"
+            ligne4 = "🔴 " + cases[6].replace(".", ",") + " cas graves " + str(
+                newCritical) + " [" + str(newCriticalPercent) + "%]\n"
         else:
-            ligne4 = "🔴 " + cases[6].replace(".", ",") + " cas graves +" + str(newCritical) + " [" + str(newCriticalPercent) + "%]\n"
-        ligne5 = "⚫ " + cases[2].replace(".", ",") + " décès +" + cases[3].replace(".", "") + " [" + str(newDeathPercent) + "%]\n"
-        ligne6 = "💉 " + cases[9].replace(".", ",") + " tests +" + str(newTests) + "\n\n"
-        ligne7 = cases[0].replace(".", ",") + " cas totaux +" + cases[1].replace(".", "")
+            ligne4 = "🔴 " + cases[6].replace(".", ",") + " cas graves +" + str(
+                newCritical) + " [" + str(newCriticalPercent) + "%]\n"
+        ligne5 = "⚫ " + cases[2].replace(".", ",") + " décès +" + cases[3].replace(
+            ".", "") + " [" + str(newDeathPercent) + "%]\n"
+        ligne6 = "💉 " + \
+            cases[9].replace(".", ",") + " tests +" + str(newTests) + "\n\n"
+        ligne7 = cases[0].replace(".", ",") + \
+            " cas totaux +" + cases[1].replace(".", "")
         ligne8 = "\n\nGraphiques📈⏬\n#ConfinementJour" + str(numberOfDay)
-        message = ligne1 + ligne2 + ligne3 + ligne4 + ligne5 + ligne6 + ligne7 + ligne8 + "\n📈Évolution du #COVID19 en 🇫🇷"
+        message = ligne1 + ligne2 + ligne3 + ligne4 + ligne5 + \
+            ligne6 + ligne7 + ligne8 + "\n📈Évolution du #COVID19 en 🇫🇷"
         msg.attach(MIMEText(message))
-
 
         mailserver = smtplib.SMTP(config.server, config.port)
         mailserver.ehlo()
